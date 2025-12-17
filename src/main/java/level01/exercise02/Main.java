42cbe57c4f6c5a7a9b96340c36f67fdd492cb813
Ana Ruth <aniarinna@gmail.com>
2025-12-17 13:01:06 +0100

    Add level 1 exercise 2(lambdas & streams)

---
+++ src/main/java/level01/exercise02/Main.java
@@ -0,0 +1,21 @@
+package level01.exercici02;
+
+import java.util.List;
+
+public class Main {
+    public static List<String>
+    StringWithLetterOAndLongerThan5(List<String> input) {
+        return input.stream()
+                .filter(s -> s.contains("o"))
+                .filter(s-> s.length() > 5)
+                .toList();
+    }
+
+    public static void main(String[] args) {
+        List<String> words = List.of("moon", "sun", "balloon", "cloud", "stormy", "rain");
+
+        List<String> result =
+                StringWithLetterOAndLongerThan5(words);
+        System.out.println(result);
+    }
+}
